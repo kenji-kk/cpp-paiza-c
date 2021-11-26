056350a2c8906f883e51176983a0a5f494907c68
kenji <kenjix12x22@gmail.com>
2021-11-26 12:16:13 +0900

    21

---
+++ 21/my.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(void){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a.rbegin(),a.rend());
+    for(int i = 0; i < n; i++){
+        cout << a[i] << endl;
+    }
+}
